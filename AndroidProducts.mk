63d8195f3a1237423c7dccb67a652434e9152948
hiper25 <2243168976@qq.com>
2019-11-20 10:22:06 -0500

    floral: Initial Arrow bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -25,7 +25,11 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_flame.mk \
     $(LOCAL_DIR)/aosp_coral_hwasan.mk \
     $(LOCAL_DIR)/aosp_flame_hwasan.mk \
+    $(LOCAL_DIR)/arrow_coral.mk \
+    $(LOCAL_DIR)/arrow_flame.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_coral-userdebug \
     aosp_flame-userdebug \
+    arrow_coral-userdebug \
+    arrow_flame-userdebug
